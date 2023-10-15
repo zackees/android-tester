a0b17e0e531ca2edf7f0a045ece8a27166db0f7b
zackees <z@zackees.com>
2023-10-15 13:18:30 -0700

    adds environment reloading

--- src/android_tester/cli.py
+++ src/android_tester/cli.py
@@ -3,9 +3,21 @@ Main entry point.
 """
 
 import argparse
+import shutil
 import sys
 
 from android_tester.android_tests import main
+
+if shutil.which("adb") is None:
+    try:
+        # Attempt to reload the environment.
+        from setenvironment import reload_environment
+
+        reload_environment()
+        if shutil.which("adb") is None:
+            raise RuntimeError("Android SDK not found.")
+    except ImportError:
+        pass
 
 
 def this_main() -> int:
